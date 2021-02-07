829f0b041ace6b3a04fd3f78e8800bd3d2512eb5
JustGinger888 <ethanmoffatt888@icloud.com>
2021-02-07 12:50:34 +0000

    upd

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ client_id='d975372af075408d9484c6347eeac929'
 client_secret='0319b5236e5c46d18a6b13a6cbaa9467'
 redirect_uri="http://127.0.0.1:5000/callback"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri))
-results = sp.current_user_saved_tracks(limit=3)
+results = sp.current_user_saved_tracks(limit=10)
 user = sp.current_user()
 playlists = sp.current_user_playlists(limit=50, offset=0)
 #----------------------------- Spotify -----------------------------#
@@ -21,7 +21,7 @@ playlists = sp.current_user_playlists(limit=50, offset=0)
 #----------------------------- Shuffle -----------------------------#
 ## Paramaters
 decade_start = '2010-01-01'
-decade_end = '2020-12-12'
+decade_end = '2019-12-12'
 artists = []
 artists_pass = False
 #albums = []
@@ -52,23 +52,22 @@ for idx, item in enumerate(results['items']):
 ## Whale
 
 ## Functionality
-#for idx, item in enumerate(results['items']):
-    #track = item['track']
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    duration = track['duration_ms']/1000
     
-    
-    #album = sp.album(track['album']['external_urls']['spotify'])
-    #features.append(track['id'])
+    album = sp.album(track['album']['external_urls']['spotify'])
+    features.append(track['id'])
 
-    #if track['album']['release_date'] >= decade_start and track['album']['release_date'] <= decade_end and track['artists'][0]['name']:
-        #output.append([idx, track['artists'][0]['name'], " – ", track['name'], track['album']['release_date']])
-        #output.append(album['genres'])
+    if track['album']['release_date'] >= decade_start and track['album']['release_date'] <= decade_end and track['artists'][0]['name']:
+        output.append([idx, track['artists'][0]['name'], track['name'], track['album']['release_date'],duration, album['genres']])
         #output.append(sp.audio_features(tracks=features[idx]))
 ## Functionality
 #----------------------------- Shuffle -----------------------------#
 
 # Algorithm
-
 # Algorithm
+
 @app.route('/')
 def welcome():
     return render_template('welcome.htm')
@@ -79,7 +78,7 @@ def login():
 
 @app.route('/shuffle')
 def shuffle():
-    return render_template('shuffle.htm',tracks=directory)
+    return render_template('shuffle.htm',tracks=output)
 
 @app.route('/profile')
 def profile():
